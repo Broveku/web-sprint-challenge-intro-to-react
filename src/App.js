22957f216b1a67f88152e8d12220aad176584250
Broveku <gearhearjordan@gmail.com>
2021-07-16 13:56:41 -0700

    working website with minimum css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import './App.css';
+import Character from './components/Character'
 
 const App = () => {
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [data, setData] = useState([])
+  
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then(res => {
+      console.log(res.data)
+      setData(res.data)
+    }).catch(err =>{
+      console.log(err)
+    })
+
+  },[])
+
+  
+
+
+
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        data.map((character, i) => {
+          return <Character key={i} info={character} />
+        })}
+        
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,71 @@
 // Write your Character component here
+import React, { useState } from 'react';
+import axios from 'axios'
+
+import styled from 'styled-components'
+
+const StyledCharacter = styled.div`
+    background-color:;
+    position:center;
+    padding:10px;
+    margin-bottom:10px;
+    
+
+    
+    
+    button{
+        margin-left:10%;
+    }
+    
+    
+`
+
+const StyledDetails = styled.div`
+    color:red;
+    background-color:black;
+    padding: 10px
+    display:flex;
+    
+
+`
+
+
+
+
+export default function Character({info}){
+    
+    const [detailsOpen, setDetailsOpen] = useState(false)
+
+    // const = id => {
+    //   setDetailsOpen(id)
+    // }
+    
+    // const  = () =>{
+    //   setDetailsOpen(null)
+    // }  
+    
+    
+    
+    return(
+        
+    <StyledCharacter>
+            {info.name}
+            <button onClick={() => setDetailsOpen(!detailsOpen)}>
+                Details
+            </button>
+        
+        
+            {detailsOpen && (
+            <StyledDetails info={info}  >
+            
+            <p>Height: {info.height}</p>
+            <p>Weight: {info.mass}</p>
+            <p>DOB: {info.birth_year}</p>
+            <p>Gender: {info.gender}</p>
+            <p>Eye Color: {info.eye_color} </p>
+        
+        </StyledDetails>)}
+    </StyledCharacter>
+        
+    )
+}
